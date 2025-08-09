44d91737fbcb0f221f60c23c701b449898e1e936
Ivan.Padilla <ivan.padilla@yum.com>
2025-08-08 21:34:05 -0700

    Initial template upload: portfolio, landing, and photography-gallery templates

--- upload-templates.sh
+++ upload-templates.sh
@@ -2,7 +2,7 @@ git init
 git add .
 git commit -m "Initial template upload: portfolio, landing, and photography-gallery templates"
 git branch -M main
-git remote add origin github https://github.com/avenirsites/avenirweb-templates.git
+git remote add origin https://github.com/avenirsites/avenirweb-templates.git
 
 # Push to GitHub
 echo "Pushing templates to GitHub..."
